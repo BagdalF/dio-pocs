49f483b4732d641c95dfb2e053d32d541ada98c3
BagdalF <bagdalfelipe@gmail.com>
2026-02-25 19:40:40 -0300

    Angular Projects

---
+++ angular/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Title 1',
+    description: 'Description 1',
+    photoCover: 'https://placehold.co/600x400',
+  },
+  {
+    id: '2',
+    title: 'Title 2',
+    description: 'Description 2',
+    photoCover: 'https://placehold.co/600x400',
+  },
+];
